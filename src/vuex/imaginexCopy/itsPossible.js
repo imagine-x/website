6c5c8f040fb81d321422549f85f3ad99e65c854e
Taylor Rhodes Singleton-Fookes <taylorsingletonfookes@live.com>
2016-10-20 22:15:10 -0700

    principles

--- src/vuex/imaginexCopy/itsPossible.js
+++ src/vuex/imaginexCopy/itsPossible.js
@@ -2,11 +2,11 @@ export const itsPossible = [
   {
     heading:['Principled Politics:'],
     variable:[
-      `Be open and genuinely transparent with everything we do - from how we communicate to the decisions we make, and the actions we take. Be genuine. Have integrity.`,
-      `Use a forward-thinking and an entrepreneurial mindset to problem-solving. Have courage to experiment responsibly. Be pro-active addressing difficult issues.`,
-      `Use a journalistic investigative approach. Sound decision-making is guided by quality facts and data. Pragmatically apply scientific rigour to evidence gathering and interpretation. Seek out objective evalution by experts.`,
-      `The purpose of government is to serve the public over private interests. Strive to create fair solutions and provide services that achieve the greatest benefits for citizens of today and tomorrow.  People oriented.  Fair oriented.`,
-      `Through respectful collaboration, we can multiply our collective contribution. We are stronger when we reach shared goals through mutual trust and support.`
+      `We can multiply our collective contribution through respectful collaboration; we are stronger when we reach shared goals through mutual trust and support.`,
+      `Use a journalistic investigative approach. Sound decision-making is guided by quality facts and data. Pragmatically apply scientific rigour to evidence gathering and interpretation. Seek out objective evalution from experts.`,
+      `Use a forward-thinking, innovative mindset to approach problems. Be pro-active when addressing difficult issues; when problems seem ingrained, have courage to experiment responsibly.`,
+      `Endeavor for community feedback during the decision-making process. Have integrity. Communicate clearly and honestly the rationale for decisions.`,
+      `The purpose of government is to put the public good over private interests. We strive to create fair solutions and provide services that achieve the greatest benefits for citizens of today and tomorrow.`,
     ],
     sources:[],
   }
