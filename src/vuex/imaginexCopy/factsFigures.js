90f0a6a0bbc3968de0be738b0d90492443557037
Taylor Rhodes Singleton-Fookes <taylorsingletonfookes@live.com>
2016-11-04 10:56:43 -0700

    remove unfinished copy from production

--- src/vuex/imaginexCopy/factsFigures.js
+++ src/vuex/imaginexCopy/factsFigures.js
@@ -1,29 +1,29 @@
 export const factsFigures = [
-{
-  url: ['/overhaul'],
-  heading:['Government needs an overhaul.'],
-  variable:[
-    `Our schools, universities, colleges, health organizations and transportation services are facing daunting challenges, but they should be able to adapt. Over the next three years transfers to BC's service delivery agencies will total over $70 billion. However they are not being managed well because we have lost control of the political narrative; our public services are being held hostage by opportunistic politicians and well funded special interest groups.`,
-    `We need a change. The people managing over $5.0 billion	for	major	transportation	capital	infrastructure need to have our confidence; in both competence and ethics.`,
-    `We do not see that in our government or in it's opposition. We see there is a need for a true alternative.`,
-  ],
-  sources:[
-    `http://bcbudget.gov.bc.ca/2016/bfp/2016_budget_and_fiscal_plan.pdf`
-  ],
-},
-{
-  url: ['/mislead'],
-  heading: [
-    "Politicians mislead intentionally."
-  ],
-  variable: [
-    "The BC Liberals have continually refused to adhere to Generally Accepted Accounting Principle, instead manipulating the provincial books to make the financial picture of the province look better than it really is.[1]",
-    "Social housing projects that the Liberals announced would be fast tracked and completed by the Olympics, were not, and no word where they are at now.[2]",
-    `Both our government and it's official opposition play games with the numbers. We need a true alternative. We need better government.`
-  ],
-  sources: [
-    "https://lailayuile.com/2013/01/06/british-columbians-deserve-easy-to-understand-financial-reportingwith-a-clean-opinion-from-their-government-auditor-general-john-doyle/",
-    "http://www.campbellcuts.com/social_housing_funding_cuts.html",
-  ],
-},
+// {
+//   url: ['/overhaul'],
+//   heading:['Government needs an overhaul.'],
+//   variable:[
+//     `Our schools, universities, colleges, health organizations and transportation services are facing daunting challenges, but they should be able to adapt. Over the next three years transfers to BC's service delivery agencies will total over $70 billion. However they are not being managed well because we have lost control of the political narrative; our public services are being held hostage by opportunistic politicians and well funded special interest groups.`,
+//     `We need a change. The people managing over $5.0 billion	for	major	transportation	capital	infrastructure need to have our confidence; in both competence and ethics.`,
+//     `We do not see that in our government or in it's opposition. We see there is a need for a true alternative.`,
+//   ],
+//   sources:[
+//     `http://bcbudget.gov.bc.ca/2016/bfp/2016_budget_and_fiscal_plan.pdf`
+//   ],
+// },
+// {
+//   url: ['/mislead'],
+//   heading: [
+//     "Politicians mislead intentionally."
+//   ],
+//   variable: [
+//     "The BC Liberals have continually refused to adhere to Generally Accepted Accounting Principle, instead manipulating the provincial books to make the financial picture of the province look better than it really is.[1]",
+//     "Social housing projects that the Liberals announced would be fast tracked and completed by the Olympics, were not, and no word where they are at now.[2]",
+//     `Both our government and it's official opposition play games with the numbers. We need a true alternative. We need better government.`
+//   ],
+//   sources: [
+//     "https://lailayuile.com/2013/01/06/british-columbians-deserve-easy-to-understand-financial-reportingwith-a-clean-opinion-from-their-government-auditor-general-john-doyle/",
+//     "http://www.campbellcuts.com/social_housing_funding_cuts.html",
+//   ],
+// },
 ]
