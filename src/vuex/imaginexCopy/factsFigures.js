081f521b7af3e01c05bc168f642911d272a40b78
melodyma <melody.yy.ma@gmail.com>
2016-11-06 18:43:13 -0800

    New ad copy for US elections

--- src/vuex/imaginexCopy/factsFigures.js
+++ src/vuex/imaginexCopy/factsFigures.js
@@ -1,6 +1,6 @@
 export const factsFigures = [
 {
-  url: ['/overhaul'],
+  url: '/overhaul',
   heading:['Government needs an overhaul.'],
   variable:[
     `Our schools, universities, colleges, health organizations and transportation services are facing daunting challenges, but they should be able to adapt. Over the next three years transfers to BC's service delivery agencies will total over $70 billion. However they are not being managed well because we have lost control of the political narrative; our public services are being held hostage by opportunistic politicians and well funded special interest groups.`,
@@ -12,7 +12,7 @@ export const factsFigures = [
   ],
 },
 {
-  url: ['/mislead'],
+  url: '/mislead',
   heading: [
     "Politicians mislead intentionally."
   ],
@@ -25,5 +25,19 @@ export const factsFigures = [
     "https://lailayuile.com/2013/01/06/british-columbians-deserve-easy-to-understand-financial-reportingwith-a-clean-opinion-from-their-government-auditor-general-john-doyle/",
     "http://www.campbellcuts.com/social_housing_funding_cuts.html",
   ],
+},
+{
+  url: '/our-turn-in-six-months',
+  heading: [
+    "BC, it's our turn in six months."
+  ],
+  variable: [
+    "The US are going to the polls today. They are making one of the most important decisions for their country and the world. But they are facing a hard choice between two of the most disliked candidates in a deeply politically polarized country.",
+    "Six months from today, British Columbians will be making the same decision for the future of our province. On May 9, 2016, it will be our turn to make a choice. We can choose between the status quo or we can choose to take our democracy back.",
+    "Let the US election remind us how fragile democracy is. It takes every one of us to step up to protect our democracy everyday. And it begins with caring. Caring for the future of our children, our environment, our economy, and all of us.",
+    "Let’s be bold and imagine a better democracy. It's possible.",
+    "We are Imagine X. We are a group of passionate citizens starting a political movement for a better BC."
+  ],
+  main: false
 },
 ]
