d4b152f36254decdcc779c2791ec06fd433ae7d3
Taylor Rhodes Singleton-Fookes <taylorsingletonfookes@live.com>
2016-11-07 22:50:20 -0800

    copy

--- src/vuex/imaginexCopy/factsFigures.js
+++ src/vuex/imaginexCopy/factsFigures.js
@@ -2,10 +2,9 @@ export const factsFigures = [
 {
   url: '/overhaul',
   heading:['Government needs an overhaul.'],
-  variable:[
-    `Our schools, universities, colleges, health organizations and transportation services are facing daunting challenges, but they should be able to adapt. Over the next three years transfers to BC's service delivery agencies will total over $70 billion. However they are not being managed well because we have lost control of the political narrative; our public services are being held hostage by opportunistic politicians and well funded special interest groups.`,
-    `We need a change. The people managing over $5.0 billion	for	major	transportation	capital	infrastructure need to have our confidence; in both competence and ethics.`,
-    `We do not see that in our government or in it's opposition. We see there is a need for a true alternative.`,
+  variable:[    
+     `Our schools, universities, colleges, health organizations and transportation services are facing daunting challenges. They need to become more agile, more efficient and more focused. Over the next three years over $70 billion will be proivided to service delivery agents in these sectors. How much of that will be squandered on administrative, bureaucratic, counter-productive activities? We have lost control of the political narrative; too often party politics, and money in party politics direct the discussion.`, 
+    `We need our policymakers to wake up to the twenty-first century. We need to take advantage of technology to make our Government better. Both our dear Liberal party and it's well intentioned opposition are unable. We need a true alternative.`,
   ],
   sources:[
     `http://bcbudget.gov.bc.ca/2016/bfp/2016_budget_and_fiscal_plan.pdf`
@@ -18,12 +17,9 @@ export const factsFigures = [
   ],
   variable: [
     "The BC Liberals have continually refused to adhere to Generally Accepted Accounting Principle, instead manipulating the provincial books to make the financial picture of the province look better than it really is.[1]",
-    "Social housing projects that the Liberals announced would be fast tracked and completed by the Olympics, were not, and no word where they are at now.[2]",
-    `Both our government and it's official opposition play games with the numbers. We need a true alternative. We need better government.`
   ],
   sources: [
     "https://lailayuile.com/2013/01/06/british-columbians-deserve-easy-to-understand-financial-reportingwith-a-clean-opinion-from-their-government-auditor-general-john-doyle/",
-    "http://www.campbellcuts.com/social_housing_funding_cuts.html",
   ],
 },
 {
@@ -32,9 +28,9 @@ export const factsFigures = [
     "BC, it's our turn in six months."
   ],
   variable: [
-    "The US is going to the polls today. They are making one of the most important decisions for their country and the world. But they are facing a hard choice between two of the most disliked candidates in a deeply politically polarized country.",
-    "Six months from today, British Columbians will be making the same decision for the future of our province. On May 9, 2016, it will be our turn to make a choice. We can choose to stick with the status quo or we can choose to take our democracy back.",
-    "Let the US election remind us how fragile democracy is. It takes every one of us to step up to protect our democracy everyday. And it begins with caring. Caring for the future of our children, our environment, our economy, and all of us.",
+    "The US is going to the polls today. In a deeply politically polarized country, they choose between two of the most disliked candidates ever on the ballot.",
+    "Let the US election remind us how fragile democracy is. It takes every one of us to step up to protect our democracy. It begins with caring. Caring for the future of our children, our environment, our economy, and all of us.",
+    "Six months from today, British Columbians will be faced with a similar choice. On May 9, 2016, it will be our turn. We can choose to stick with the right or the left of the status quo or we can choose to take our democracy back.",
     "Let’s be bold and imagine a better democracy. It's possible.",
     "We are Imagine X. We are a group of passionate citizens starting a political movement for a better BC."
   ],
