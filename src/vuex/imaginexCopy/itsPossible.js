40516603695bfae3f365e70ce41159cea651f2f4
Melody Ma <melodyma@users.noreply.github.com>
2016-11-09 07:52:20 -0800

    Date change

--- src/vuex/imaginexCopy/itsPossible.js
+++ src/vuex/imaginexCopy/itsPossible.js
@@ -33,7 +33,7 @@ export const itsPossible = [{
     variable: [
       "What happened. This is no longer a joke.",
       "Let the US election remind us how fragile democracy is. It takes every one of us to step up to protect our democracy. It begins with caring. Caring for the future of our children, our environment, our economy, and all of us.",
-      "Six months from today, British Columbians will be faced with a similar choice. On May 9, 2016, it will be our turn. We can choose to stick with the right or the left of the status quo or we can choose to take our democracy back.",
+      "Six months from today, British Columbians will be faced with a similar choice. On May 9, 2017, it will be our turn. We can choose to stick with the right or the left of the status quo or we can choose to take our democracy back.",
       "Let’s be bold and imagine a better democracy. It's possible.",
       "We are Imagine X. We are a group of passionate citizens starting a political movement for a better BC."
     ],
@@ -46,7 +46,7 @@ export const itsPossible = [{
     variable: [
       "The US is going to the polls today. They are choosing between two of the most disliked candidates in a deeply politically polarized country.",
       "Let the US election remind us how fragile democracy is. It takes every one of us to step up to protect our democracy. It begins with caring. Caring for the future of our children, our environment, our economy, and all of us.",
-      "Six months from today, British Columbians will be faced with a similar choice. On May 9, 2016, it will be our turn. We can choose to stick with the right or the left of the status quo or we can choose to take our democracy back.",
+      "Six months from today, British Columbians will be faced with a similar choice. On May 9, 2017, it will be our turn. We can choose to stick with the right or the left of the status quo or we can choose to take our democracy back.",
       "Let’s be bold and imagine a better democracy. It's possible.",
       "We are Imagine X. We are a group of passionate citizens starting a political movement for a better BC."
     ],
