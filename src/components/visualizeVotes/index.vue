26de30c0e8ea3689049dbb7222bfb78f6d8efe95
melodyma <melody.yy.ma@gmail.com>
2016-11-22 00:53:34 -0800

    Copy update

--- src/components/visualizeVotes/index.vue
+++ src/components/visualizeVotes/index.vue
@@ -9,7 +9,7 @@ div
       ul
         li Each rectangle represents a vote in the legislature between 2013 to 2016 for the 40th parliament under the B.C. Liberal government. A vote can be a motion for an ammendment, bill or other matters. Click on the rectangle to discover the details of each vote.
         li Each dot represents an MLA who voted. The colour of the dot represents the MLA's party affiliation.
-        li The top half of the graph represents 'YAY' votes. The bottom of the graph represents 'NAY votes'. Votes are passed if there are more 'YAY' votes than 'NAY' votes.
+        li The top half of the graph represents 'YAY' votes. The bottom of the graph represents 'NAY votes. Votes are passed if there are more 'YAY' votes than 'NAY' votes.
     hr
   div.body-container
     span Sort by: &nbsp
