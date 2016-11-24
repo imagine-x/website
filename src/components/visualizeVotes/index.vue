aa169da881fa9535456a26ef8f964f470cf972a3
melodyma <melody.yy.ma@gmail.com>
2016-11-23 19:45:05 -0800

    Updated none

--- src/components/visualizeVotes/index.vue
+++ src/components/visualizeVotes/index.vue
@@ -264,7 +264,7 @@ export default {
                     });
 
                     mlaSelect.addEventListener('change', function(e){
-                        mlaSelected = this.value;
+                        mlaSelected = (this.value !== '0') ? this.value : null;
                         self.init(bills, mlaSelected);
                     });
 
