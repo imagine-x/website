142c8f84f1043d408d8c8e4a67bf52c2ddeec2c8
melodyma <melody.yy.ma@gmail.com>
2016-12-30 16:50:32 -0800

    added locations object

--- src/assets/locationData.js
+++ src/assets/locationData.js
@@ -1,3 +1,48 @@
+export const locations = [{
+        name: 'Richmond',
+        ridings: ["Richmond Centre","Richmond-Steveston","Richmond East"]
+    }, {
+        name: 'Burnaby',
+        ridings: ["Burnaby North","Burnaby-Willingdon","Burnaby-Edmonds"]
+    }, {
+        name: 'Cariboo-Thompson',
+        ridings: ["Cariboo North","Cariboo South","Yale-Lillooet","Kamloops","Kamloops–North Thompson"]
+    }, {
+        name: 'Columbia-Kootenay',
+        ridings:["Columbia River–Revelstoke","East Kootenay","Nelson-Creston","West Kootenay–Boundary"]
+    }, {
+        name: 'Delta',
+        ridings: ["Delta South", "Delta North"]
+    },  {
+        name: 'Fraser Valley',
+        ridings: ["Maple Ridge–Pitt Meadows","Maple Ridge–Mission","Langley","Fort Langley–Aldergrove","Abbotsford–Mount Lehman","Abbotsford-Clayburn","Chilliwack-Sumas","Chilliwack-Kent"]
+    }, {
+        name: 'New Westminster',
+        ridings: ["New Westminster"]
+    }, {
+        name: 'North Shore',
+        ridings: ["North Vancouver–Seymour","North Vancouver–Lonsdale","West Vancouver–Capilano","West Vancouver–Garibaldi"]
+    }, {
+        name: 'Okanagan',
+        ridings: ["Shuswap","Okanagan-Vernon","Kelowna–Lake Country","Kelowna-Mission","Okanagan-Westside","Penticton–Okanagan Valley"]
+    }, {
+        name: 'Surrey',
+        ridings: ["Surrey-Whalley","Surrey-Green Timbers","Surrey-Tynehead","Surrey-Newton","Surrey–Panorama Ridge","Surrey-Cloverdale","Surrey–White Rock"]
+    }, {
+        name: 'The North',
+        ridings: ['North Coast','Skeena','Bulkley Valley–Stikine','Prince George–Omineca','Prince George North','Prince George–Mount Robson','Peace River South','Peace River North']
+    }, {
+        name: 'Tri-Cities',
+        ridings: ["Port Moody–Westwood","Burquitlam","Coquitlam-Maillardville","Port Coquitlam–Burke Mountain"]
+    }, {
+        name: 'Vancouver',
+        ridings: ["Vancouver-Hastings","Vancouver-Kingsway","Vancouver-Fraserview","Vancouver–Mount Pleasant","Vancouver-Kensington","Vancouver-Burrard","Vancouver-Fairview","Vancouver-Langara","Vancouver–Point Grey","Vancouver-Quilchena"]
+    }, {
+        name: 'Vancouver Island and South Coast',
+        ridings: ["Powell River–Sunshine Coast","North Island","Comox Valley","Alberni-Qualicum","Nanaimo-Parksville","Nanaimo","Cowichan-Ladysmith","Malahat–Juan de Fuca","Saanich North and the Islands","Saanich South","Oak Bay–Gordon Head","Victoria–Beacon Hill","Victoria-Hillside","Esquimalt-Metchosin"]
+    }
+]
+
 export const ridings = [ 'Abbotsford South',
 'Abbotsford West',
 'Abbotsford-Mission',
