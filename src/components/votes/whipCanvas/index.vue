fc9f44b476f0532bd2ea802e013b952dff83d93c
melodyma <melody.yy.ma@gmail.com>
2016-12-30 19:16:06 -0800

    Updated canvas overflow styling

--- src/components/votes/whipCanvas/index.vue
+++ src/components/votes/whipCanvas/index.vue
@@ -65,7 +65,7 @@ export default {
     width: 3rem;
     height: 110%;
     z-index: z-index-1;
-    margin-right: 1rem;
+    margin-right: 2rem;
     transform: translateY(-100%);
     right: 0;
     background: linear-gradient(to right,rgba(255,255,255,0), white);
@@ -78,7 +78,7 @@ export default {
     width: 3rem;
     height: 110%;
     z-index: z-index-1;
-    margin-left: 2.5rem;
+    margin-left: 2rem;
     left: 0;
     background: linear-gradient(to left,rgba(255,255,255,0), white);
 }
